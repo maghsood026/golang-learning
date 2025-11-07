27b3ad5e48e2514085b1da90c8a7f66be149731e
maghsood95 <maghsood95@gmail.com>
2025-11-07 10:40:26 +0330

    working on loop and add examples

--- ch4/main.go
+++ ch4/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func shadowing() {
 	x := 10
@@ -15,8 +18,49 @@ func shadowing() {
 		fmt.Println("what the hell is shadowing")
 	}
 }
+
+func conditionGo() {
+	if n := rand.Intn(10); n == 0 {
+		fmt.Println(n, "number is too short")
+	} else if n > 5 {
+		fmt.Println(n, "number is big enough")
+	} else {
+		fmt.Println(n, "the number is good right now")
+	}
+}
+func loopOne() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+func loopTwo() {
+	i := -10
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+}
+
+func loopThree() {
+	for {
+		fmt.Println("hello")
+	}
+}
+
+func loopFour() {
+	var mynumbers []int = []int{1, 2, 3, 4, 5}
+	for _, v := range mynumbers {
+		fmt.Println(v)
+	}
+}
 func main() {
-	
-	shadowing()
+
+	// shadowing()
+	// conditionGo()
+	// loopOne()
+	// loopTwo()
+	// loopThree()
+	loopFour()
 
 }
